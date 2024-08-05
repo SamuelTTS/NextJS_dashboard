a92369d684dab36b0037f8e1ebad60f135441dc7
Samuel TCHABLINTETE <stchablintete@gmail.com>
2024-08-05 11:29:00 +0000

    ajout du boutton dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,9 @@ export default function Page() {
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+        <Link href={"/dashboard"}
+          className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+          >dashboard</Link>
           <Image
             src="/hero-desktop.png"
             width={1000}
@@ -39,6 +42,7 @@ export default function Page() {
           >
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
+          
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
